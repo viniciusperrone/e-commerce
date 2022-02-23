62d1f3753bc1c39600829c86008fc1778d4c84ce
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-02-23 19:52:30 -0300

    adding new templates

--- store/models.py
+++ store/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 from django.conf import settings
 
-# Create your models here.
+CATEGORY_CHOICES = (
+    ('HS', 'Headset'),
+    ('MC', 'Mic'),
+    ('SB', 'Sound Box'),
+    ('KB', 'Keyboard'),
+    ('MS', 'Mouse'),
+    ('MB', 'Motherboard'),
+    ('HD', 'Hard Drive')
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices= LABEL_CHOICES, max_length=1)
     def __str__(self):
         return self.title
 
