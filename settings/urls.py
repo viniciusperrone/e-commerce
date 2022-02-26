7a7ed5f0a812d9664405b6b30963d12d20aeb13c
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-02-26 11:34:22 -0300

    upload and new fiels in model

--- settings/urls.py
+++ settings/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('store.urls'))
-] + static(settings.MEDIA_URL, document=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
